405f482e6da7f8ecea9427d784946a34251a2511
Mohamed Al Glawi <shadowrapplique@gmail.com>
2023-11-01 16:47:30 +0100

    fix handler

--- shared/legendUtils/baseHandler.cpp
+++ shared/legendUtils/baseHandler.cpp
@@ -3,7 +3,7 @@
 BaseHandler::BaseHandler(std::shared_ptr<RouterInterface> router, std::shared_ptr<DeviceManager> deviceManager)
 {
     this->router_ = router;
-    this->deviceManger_ = deviceManager;
+    this->deviceManager_ = deviceManager;
 }
 
 /*void BaseHandler::handle(Request *request)
