c0ae3c7d22b85e606de00d2ea968fcba89fae9b3
Mohamed Al Glawi <shadowrapplique@gmail.com>
2023-11-04 15:08:45 +0100

    first poke with serialib

--- nowlegend-bridge/src/main.cpp
+++ nowlegend-bridge/src/main.cpp
@@ -1,19 +1,36 @@
 #include <websocketpp/config/asio_no_tls_client.hpp>
 #include <websocketpp/client.hpp>
- 
 #include <websocketpp/common/thread.hpp>
 #include <websocketpp/common/memory.hpp>
 
+#include <serialib.h>
+
 #include <cstdlib>
+
 #include <iostream>
 #include <map>
 #include <string>
 #include <sstream>
 
+
+#define SERIAL_PORT "/dev/ttyUSB0"
+
 int main() {
     //int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    while(true) {
+    serialib serial;
+    char buffer[100] {};
+    char errorOpening = serial.openDevice(SERIAL_PORT, 115200);
+    if (errorOpening!=1) return errorOpening;
+    std::cout << "Successful connection to %s\n",SERIAL_PORT;
 
+    while(true) {
+        serial.readString(buffer, '\n', 100, 2000);
+        std::cout << buffer << "\n";
     }
+
+
+    /*while(true) {
+
+    }*/
     return 0; 
 }
