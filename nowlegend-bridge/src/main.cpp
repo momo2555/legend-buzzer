8735f3fd2c202aabd6836e1ce6edfd2352f9bfce
Mohamed Al Glawi <shadowrapplique@gmail.com>
2023-11-04 14:01:52 +0100

    delete unusfull cmakelists

--- nowlegend-bridge/src/main.cpp
+++ nowlegend-bridge/src/main.cpp
@@ -1,7 +1,19 @@
-#include <sys/socket.h>
+#include <websocketpp/config/asio_no_tls_client.hpp>
+#include <websocketpp/client.hpp>
+ 
+#include <websocketpp/common/thread.hpp>
+#include <websocketpp/common/memory.hpp>
 
-main() {
-    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    
+#include <cstdlib>
+#include <iostream>
+#include <map>
+#include <string>
+#include <sstream>
+
+int main() {
+    //int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    while(true) {
+
+    }
     return 0; 
 }
